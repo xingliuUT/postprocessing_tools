cd7c9dc2b08fc14032dccde9a7e79eaf4827c4ca
xingliu <xingliu@utexas.edu>
2017-08-02 07:37:19 -0700

    Read geometry file into two dictionaries

---
+++ geomHelper.py
@@ -0,0 +1,11 @@
+from parIOHelper import init_read_parameters
+from read_write_geometry import *
+
+def init_read_geometry_file(suffix, pars):
+
+    geom_type = pars['magn_geometry'][1:-1]
+    geom_file = geom_type + suffix
+    geom_pars, geom_coeff = read_geometry_local(geom_file)
+
+    return geom_type, geom_pars, geom_coeff
+
