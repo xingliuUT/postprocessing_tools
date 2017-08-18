6ad151d7e000bc0b461784e8ed31b150b16a3246
xingliu <xingliu@utexas.edu>
2017-08-17 20:46:58 -0700

    compute outer product of q*Cy and zgrid

---
+++ TestDrive_momHelper.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+from fieldHelper import *
+from momHelper import *
+from parIOHelper import *
+from geomHelper import *
+from plotHelper import *
+from windowFFT import *
+import sys
+
+suffix = sys.argv[1]
+
+if not suffix =='.dat':
+   suffix = '_'+suffix
+
+pars = init_read_parameters(suffix)
+field = fieldfile('field'+suffix,pars)
+#field_step_time(field)
+momen = momfile('mom_e'+suffix,pars)
+#momen_step_time(momen)
+
+nz = pars['nz0']
+nx = pars['nx0']
+dz = 2.0/nz
+zgrid = np.arange(nz)/float(nz-1)*(2.0-dz)-1.0
+if 'lx_a' in pars:
+    xgrid = np.arange(nx)/float(nx-1)*pars['lx_a']+pars['x0']-pars['lx_a']/2.0
+else:
+    xgrid = np.arange(nx)/float(nx-1)*pars['lx'] - pars['lx']/2.0
+
+show_plots = True
+plot_format = 'display'
+#plot_format = 'ps'
+nf = 200
+lf = 10.
+
+tStart = 350.
+tEnd = 390.
+kygrid = range(pars['nky0'])
+#kygrid = [0]
+zInd = nz/2
+kyInd = -1
+xInd = nx*5/8
+ky = 0
+
+geom_type, geom_pars, geom_coeff = init_global_geometry(suffix, pars)
+q, Cy = q_Cy(geom_coeff)
+qCy = np.array(q * Cy)
+print np.shape(q), np.shape(Cy), np.shape(qCy)
+print np.shape(zgrid)
+ymatrix = np.outer(zgrid*np.pi, qCy)
+print np.shape(ymatrix)
+#filename = '0'
+#doublePlot1D(xgrid, q, Cy, 'q', 'Cy', 'geometry coeff', filename, plot_format)
+#if 1 == 1:
+dens_xz = np.zeros((nz, nx),dtype='complex128')
+for ky in kygrid:
+    time, this_dens, this_tperp = global_moments(momen, -1, ky, -1)
+    print np.shape(this_dens)
+#    if show_plots:
+    if 1 == 0:
+        title = 'time =' + str(np.round(time,1)) + ', ky index =' + str(ky)
+        filename = 'dens_Tperp_ky='+str(ky)+'.ps'
+        doublePlot2D(xgrid, zgrid, dens, tperp, 'dens', 'tperp', title, filename, 'x', 'z',plot_format)
