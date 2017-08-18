9f708f88e796ee72e296f1f2cb6253fc5736ef35
xingliu <xingliu@utexas.edu>
2017-08-17 21:01:21 -0700

    compute dens vs x,z for all ky, zeta=0

--- TestDrive_momHelper.py
+++ TestDrive_momHelper.py
@@ -21,7 +21,7 @@ field = fieldfile('field'+suffix,pars)
 #field_step_time(field)
 momen = momfile('mom_e'+suffix,pars)
 #momen_step_time(momen)
-
+zi = complex(0, 1)
 nz = pars['nz0']
 nx = pars['nx0']
 dz = 2.0/nz
@@ -49,19 +49,22 @@ ky = 0
 geom_type, geom_pars, geom_coeff = init_global_geometry(suffix, pars)
 q, Cy = q_Cy(geom_coeff)
 qCy = np.array(q * Cy)
-print np.shape(q), np.shape(Cy), np.shape(qCy)
-print np.shape(zgrid)
+#print np.shape(q), np.shape(Cy), np.shape(qCy)
+#print np.shape(zgrid)
 ymatrix = np.outer(zgrid*np.pi, qCy)
-print np.shape(ymatrix)
+#print np.shape(ymatrix)
 #filename = '0'
 #doublePlot1D(xgrid, q, Cy, 'q', 'Cy', 'geometry coeff', filename, plot_format)
 #if 1 == 1:
 dens_xz = np.zeros((nz, nx),dtype='complex128')
+dens_naive = np.zeros((nz, nx),dtype='complex128')
 for ky in kygrid:
     time, this_dens, this_tperp = global_moments(momen, -1, ky, -1)
-    print np.shape(this_dens)
+    dens_xz += np.multiply(this_dens, np.exp(zi * ky * ymatrix))
+    dens_naive += this_dens
+#    print np.shape(this_dens)
 #    if show_plots:
-    if 1 == 0:
-        title = 'time =' + str(np.round(time,1)) + ', ky index =' + str(ky)
-        filename = 'dens_Tperp_ky='+str(ky)+'.ps'
-        doublePlot2D(xgrid, zgrid, dens, tperp, 'dens', 'tperp', title, filename, 'x', 'z',plot_format)
+if 1 == 1:
+    title = 'time =' + str(np.round(time,1))
+    filename = 'dens01.ps'
+    doublePlot2D(xgrid, zgrid, dens_xz, dens_naive, 'dens_xz', 'dens_naive', title, filename, 'x', 'z',plot_format)
