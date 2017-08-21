f3c1301c5f4a248c431a8bd7365d80013a773719
Xing Liu <xl3676@c442-402.stampede.tacc.utexas.edu>
2017-08-21 12:06:27 -0500

    plot dens vs f,ky for various x

--- TestDrive_fieldHelper.py
+++ TestDrive_fieldHelper.py
@@ -19,44 +19,41 @@ tStart = float(sys.argv[2])
 tEnd = float(sys.argv[3])
 
 pars = init_read_parameters(suffix)
-field = fieldfile('field'+suffix,pars)
-#field_step_time(field)
 momen = momfile('mom_e'+suffix,pars)
 #momen_step_time(momen)
 
 nz = pars['nz0']
 nx = pars['nx0']
-dz = 2.0/nz
-zgrid = np.arange(nz)/float(nz-1)*(2.0-dz)-1.0
 if 'lx_a' in pars:
     xgrid = np.arange(nx)/float(nx-1)*pars['lx_a']+pars['x0']-pars['lx_a']/2.0
 else:
     xgrid = np.arange(nx)/float(nx-1)*pars['lx'] - pars['lx']/2.0
 
-show_plots = True
-plot_format = 'display'
-#plot_format = 'ps'
+#plot_format = 'display'
+plot_format = 'ps'
 nf = 400
 lf = 20.
+minVal = 1.
 
-
+#kygrid = np.array(range(pars['nky0'])) * pars['kymin']
 kygrid = range(pars['nky0'])
-#kygrid = [0]
+
 zInd = nz/2
 kyInd = -1
-xInd = nx * 5/8
-ky = 0
-if 1 == 1:
+#xInd = nx / 2
+for xInd in range(nx/2, nx * 3 / 4, 8):
+    print xgrid[xInd]
     tgrid, dens_tky, tperp_tky = momen_tky(momen,zInd,kyInd,xInd,tStart,tEnd)
-    if 1 == 1:
+    if 1 == 0:
         title = 'xgrid: time, ygrid: ky, x ='+str(xgrid[xInd])
         filename = 'tky_dens_tperp.ps'
         doublePlot2D(kygrid, tgrid, dens_tky, tperp_tky, 'dens', 'tperp', title, filename, 'ky', 't', plot_format)
 
     fgrid, dens_fky = momen_fx(dens_tky, tgrid, nf, lf)
-    fgrid, tperp_fky = momen_fx(tperp_tky, tgrid, nf, lf)
+#    fgrid, tperp_fky = momen_fx(tperp_tky, tgrid, nf, lf)
     if 1 == 1:
-        title = 'xgrid: frequency, ygrid: x, all ky\'s added'
-        filename = 'fky_dens_tperp.ps'
-        doublePlot2D(kygrid, fgrid, dens_fky, tperp_fky, 'dens', 'tperp', title, filename, 'ky', 'f', plot_format)
+        title = ' '
+        filename = 'fky_dens_x='+str(xgrid[xInd])+'.ps'
+#        doublePlot2D(kygrid, fgrid, dens_fky, tperp_fky, 'dens', 'tperp', title, filename, 'ky', 'f', plot_format)
+        singlePlot2D(kygrid, fgrid, dens_fky, 'dens', title, filename, 'ky', 'f', plot_format)
 
