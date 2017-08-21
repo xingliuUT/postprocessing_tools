611edb73a621ade3db5cea75be97bd2dff854274
Xing Liu <xl3676@c442-402.stampede.tacc.utexas.edu>
2017-08-21 12:05:14 -0500

    clean up momen_fky and momen_fx methods

--- momHelper.py
+++ momHelper.py
@@ -19,8 +19,6 @@ def global_moments(momen, \
     nz = momen.pars['nz0']
     nky = momen.pars['nky0']
     nx = momen.pars['nx0']
-#    deln = np.zeros((nz,nx),dtype='complex128')
-#    tperp = np.zeros((nz,nx),dtype='complex128')
     if zInd == -1 and kyInd != -1 and xInd == -1: 
         deln = momen.dens()[0 : nz, kyInd, 0 : nx]
         tperp = momen.tperp()[0 : nz, kyInd, 0 : nx]
@@ -30,30 +28,7 @@ def global_moments(momen, \
     elif zInd != -1 and kyInd == -1 and  xInd != -1:
         deln = momen.dens()[zInd, 0 : nky, xInd]
         tperp = momen.tperp()[zInd, 0 : nky, xInd]
-#    print(np.shape(deln), np.shape(tperp))
     return time, deln, tperp
-
-def t_avg_global_moms(momen, \
-                      kyInd = 0, \
-                      tStart = 0., \
-                      tEnd = 0.):
-
-    itStart = np.argmin(abs(np.array(momen.tmom)-tStart))
-    itEnd = np.argmin(abs(np.array(momen.tmom)-tEnd))
-
-    nz = momen.pars['nz0']
-    nx = momen.pars['nx0']
-
-    deln = np.zeros((nz,nx),dtype='complex128')
-    tperp = np.zeros((nz,nx),dtype='complex128')
-
-    for timeInd in range(itStart, itEnd + 1):
-        t, this_deln, this_tperp = global_moments(momen, kyInd, timeInd)
-        deln += this_deln
-        tperp += this_tperp
-
-    return deln / (itEnd - itStart + 1), tperp / (itEnd - itStart + 1)
-
 def momen_step_time(momen, \
               show_plots = True):
     momen_time = momen.tmom
@@ -70,8 +45,8 @@ def momen_tx(momen, \
                   xgrid, \
                   zInd, \
                   tStart, \
-                  tEnd):
-
+                  tEnd, \
+                  show_xz = False):
     itStart = np.argmin(abs(np.array(momen.tmom)-tStart))
     itEnd = np.argmin(abs(np.array(momen.tmom)-tEnd))
     tsteps = itEnd - itStart + 1
@@ -80,17 +55,15 @@ def momen_tx(momen, \
     nx = momen.pars['nx0']
     deln_tx = np.zeros((tsteps, nx),dtype='complex128')
     tperp_tx = np.zeros((tsteps, nx),dtype='complex128')
-
     for timeInd in range(itStart, itEnd + 1):
         deln_x = np.zeros(nx,dtype='complex128')
         tperp_x = np.zeros(nx,dtype='complex128')
-        time, dens_xz, tperp_xz = momen_xz(momen, geom_coeff, zgrid, kygrid, xgrid, timeInd, True)
+        if show_xz:
+            time, dens_xz, tperp_xz = momen_xz(momen, geom_coeff, zgrid, kygrid, xgrid, timeInd, True)
+        else:
+            time, dens_xz, tperp_xz = momen_xz(momen, geom_coeff, zgrid, kygrid, xgrid, timeInd)
         deln_x = dens_xz[zInd,:]
         tperp_x = tperp_xz[zInd,:]
-        #for ky in kygrid:
-            #time, this_deln, this_tperp = global_moments(momen, zInd, ky, xInd, timeInd)
-            #deln_x += this_deln
-            #tperp_x += this_tperp
         deln_tx[timeInd - itStart, :] = deln_x.reshape(1, nx)
         tperp_tx[timeInd - itStart, :] = tperp_x.reshape(1, nx)
         tgrid.append(time)
@@ -102,40 +75,27 @@ def momen_fx(field_tx, tgrid, nf, lf):
     for i in range(nx):
         fgrid, field_f = windowFFT(np.array(tgrid), field_tx[:,i], nf, lf, str(i/float(nx)))
         field_f = field_f.reshape(nf)
-        for j in range(nf):
-            field_f[j] = np.minimum(field_f[j], 100)
-        field_fx[:, i] = field_f
-        
+        field_fx[:,i] = field_f
     return fgrid, field_fx
-
 def momen_tky(momen, \
                   zInd, \
                   kyInd, \
                   xInd, \
                   tStart, \
                   tEnd):
-
+    nky = momen.pars['nky0']
     itStart = np.argmin(abs(np.array(momen.tmom)-tStart))
     itEnd = np.argmin(abs(np.array(momen.tmom)-tEnd))
     tsteps = itEnd - itStart + 1
     tgrid = []
-    nz = momen.pars['nz0']
-    nky = momen.pars['nky0']
-    nx = momen.pars['nx0']
     deln_tky = np.zeros((tsteps, nky),dtype='complex128')
     tperp_tky = np.zeros((tsteps, nky),dtype='complex128')
-
     for timeInd in range(itStart, itEnd + 1):
-        deln_ky = np.zeros(nky,dtype='complex128')
-        tperp_ky = np.zeros(nky,dtype='complex128')
         time, this_deln, this_tperp = global_moments(momen, zInd, kyInd, xInd, timeInd)
         deln_tky[timeInd - itStart, :] = this_deln.reshape(1, nky)
         tperp_tky[timeInd - itStart, :] = this_tperp.reshape(1, nky)
         tgrid.append(time)
-    print(len(tgrid))
-    print(np.shape(deln_tky), np.shape(tperp_tky))
     return tgrid, deln_tky, tperp_tky
-
 def momen_xz(momen, geom_coeff, zgrid, kygrid, xgrid, timeInd = -1, show_plots = False):
 
     q, Cy = q_Cy(geom_coeff)
