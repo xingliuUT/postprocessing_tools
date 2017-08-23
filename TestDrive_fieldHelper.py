5fb33d9ae6162cb78b76a8648464e141ebaeb6a7
Xing Liu <xl3676@c442-304.stampede.tacc.utexas.edu>
2017-08-23 16:34:31 -0500

    plot phi,apar vs x,z

--- TestDrive_fieldHelper.py
+++ TestDrive_fieldHelper.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from fieldHelper import *
 from momHelper import *
+from geomHelper import *
 from parIOHelper import *
 from plotHelper import *
 from windowFFT import *
@@ -15,14 +16,18 @@ suffix = sys.argv[1]
 if not suffix =='.dat':
    suffix = '_'+suffix
 
+zi = complex(0,1)
 tStart = float(sys.argv[2])
 tEnd = float(sys.argv[3])
 
 pars = init_read_parameters(suffix)
 field = fieldfile('field' + suffix, pars)
 
+geom_type, geom_pars, geom_coeff = init_global_geometry(suffix, pars)
 nz = pars['nz0']
 nx = pars['nx0']
+dz = 2.0/nz
+zgrid = np.arange(nz)/float(nz-1)*(2.0-dz)-1.0
 if 'lx_a' in pars:
     xgrid = np.arange(nx)/float(nx-1)*pars['lx_a']+pars['x0']-pars['lx_a']/2.0
 else:
@@ -35,11 +40,6 @@ kygrid = range(pars['nky0'])
 
 zInd = -1
 xInd = -1
+
 timeInd = np.argmin(abs(np.array(field.tfld) - tStart))
-for ky in kygrid:
-    time, phi_xz, apar_xz = global_eigenfunctions(field, zInd, ky, xInd, timeInd)
-    title = 'ky=' + str(ky)
-    filename = 'n='+str(ky*6)+'_phi_apar_time='+str(np.round(time,4))+'.ps'
-    doublePlot2D(xgrid, zgrid, phi_xz, apar_xz, 'phi_xz', 'apar_xz', title, filename, 'x', 'z', plot_format)
-
-
+time, phi_xz, apar_xz = field_xz(field, geom_coeff, zgrid, kygrid, xgrid, timeInd, True, plot_format)
