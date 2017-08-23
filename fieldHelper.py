4bf0ba7f13f88428ca6c2a55c57bc1dfefbf616d
Xing Liu <xl3676@c442-304.stampede.tacc.utexas.edu>
2017-08-23 14:38:47 -0500

    multiply rhostar to get phi/Tref/e, apar/Bref/rhoref

--- fieldHelper.py
+++ fieldHelper.py
@@ -5,43 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fieldlib import *
 
-def global_eigenfunctions(field, \
-                          zInd = -1, \
-                          kyInd = 0, \
-                          xInd = -1, \
-                          setTime = -1):
-    field.set_time(field.tfld[setTime])
-    time = field.tfld[setTime]
-    print 'Reading eigenfunctions are at t = ', time
-    nz = field.nz
-    nx = field.nx
-    phi = np.zeros((nz, nx), dtype='complex128')
-    apar = np.zeros((nz, nx), dtype='complex128')
-    if zInd == -1 and xInd == -1:
-        phi = field.phi()[0 : nz, kyInd, 0 : nx]
-        apar = field.apar()[0 : nz, kyInd, 0 : nx]
-    elif xInd == -1:
-        phi = field.phi()[zInd, kyInd, 0 : nx]
-        apar = field.apar()[zInd, kyInd, 0 : nx]
-    return time, phi, apar
-
-def t_avg_global_eigenfns(field, \
-                          kyInd = 0, \
-                          tStart = 0, \
-                          tEnd = 0):
-    itStart = np.argmin(abs(np.array(field.tfld) - tStart))
-    itEnd = np.argmin(abs(np.array(field.tfld) - tEnd))
-    phi = np.zeros((field.nz, field.nx), dtype='complex128')
-    apar = np.zeros((field.nz, field.nx), dtype='complex128')
-    for timeInd in range(itStart, itEnd + 1):
-        t, this_phi, this_apar = global_eigenfunctions(field, \
-                                 kyInd, timeInd)
-        phi += this_phi
-        apar += this_apar
-    return phi / (itEnd - itStart + 1), apar / (itEnd - itStart + 1)
-
 def field_step_time(field, \
-              show_plots = True):
+                    show_plots = True):
     field_time = field.tfld
     step_time = np.array(field_time[1:-1]) - np.array(field_time[0:-2])
     if show_plots:
@@ -49,7 +14,29 @@ def field_step_time(field, \
         plt.title('Phi and Apar')
         plt.ylabel('step time (Lref / cref)')
         plt.show()
-
+def global_eigenfunctions(field, \
+                          zInd, \
+                          kyInd, \
+                          xInd, \
+                          setTime = -1):
+    field.set_time(field.tfld[setTime])
+    time = field.tfld[setTime]
+    print 'Reading eigenfunctions are at t = ', time
+    nz = field.nz
+    ny = field.ny
+    nx = field.nx
+    if zInd == -1 and kyInd != -1 and xInd == -1:
+        phi = field.phi()[0 : nz, kyInd, 0 : nx]
+        apar = field.apar()[0 : nz, kyInd, 0 : nx]
+    elif zInd != -1 and kyInd != -1 and xInd == -1:
+        phi = field.phi()[zInd, kyInd, 0 : nx]
+        apar = field.apar()[zInd, kyInd, 0 : nx]
+    elif zInd != -1 and kyInd == -1 and xInd != -1:
+        phi = field.phi()[zInd, 0:ny, xInd]
+        apar = field.apar()[zInd, 0:ny, xInd]
+    phi = phi*field.pars['rhostar']
+    apar = apar*field.pars['rhostar']
+    return time, phi, apar
 def field_tx(field, \
                   zInd, \
                   kygrid, \
