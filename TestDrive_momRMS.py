f386057fbf3e49164cdbc9bb5becc75246fd1612
Xing Liu <xingliu@utexas.edu>
2017-08-25 17:46:39 -0500

    clean up unrelated lines

--- TestDrive_momRMS.py
+++ TestDrive_momRMS.py
@@ -58,24 +58,4 @@ if 1 == 1:
     plot_format = 'ps'
     for xInd in range(nx / 4, nx * 3 / 4 + 1, 8):
         t_rms, tperp_rms = momen_rms(tgrid, tperp_tx, xInd, show_plots, plot_format)
-if 1 == 0:
-    fgrid, dens_fx = momen_fx(dens_tx, tgrid, nf, lf)
-    fgrid, tperp_fx = momen_fx(tperp_tx, tgrid, nf, lf)
-#    title = 'tStart='+str(tStart)+', tEnd='+str(tEnd)
-    np.savetxt('dens_fx.txt', dens_fx.view(float))
-    np.savetxt('tperp_fx.txt', tperp_fx.view(float))
-    np.savetxt('fgrid.txt', fgrid)
-    title = ' '
-    filename = 'phi_fx01.ps'
-#    singlePlot2D(xgrid, fgrid, dens_fx, 'phi_fx', title, filename, 'x', 'f',plot_format)
-    doublePlot2D(xgrid, fgrid, dens_fx, tperp_fx, 'phi_fx', 'apar_fx', title, filename, 'x', 'f',plot_format)
-
-    for i in range(nx / 2, nx * 3 / 4, 8):
-        dens_f = dens_fx[:,i]
-
-        plt.plot(fgrid, abs(dens_f))
-        plt.ylabel('abs phi')
-        plt.xlabel('f')
-        plt.title(str(i))
-        plt.show()
 
