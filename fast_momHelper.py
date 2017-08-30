c1faa31073621ef3e19d2d0a661dbc705498590b
xingliu <xingliu@utexas.edu>
2017-08-30 08:29:04 -0700

    remove variables that are not used

--- fast_momHelper.py
+++ fast_momHelper.py
@@ -36,15 +36,10 @@ else:
 show_plots = True
 plot_format = 'display'
 #plot_format = 'ps'
-nf = 200
-lf = 1.
 
 kygrid = range(pars['nky0'])
 #kygrid = [0]
-zInd = 33
-kyInd = -1
-xInd = nx*5/8
-ky = 0
+zInd = nz / 2
 
 if 1 == 1:
     tgrid, dens_tx, tperp_tx = momen_tx(momen, \
