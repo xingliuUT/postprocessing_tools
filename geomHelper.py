7dd6281ff61e7fa7156836f4cc8a53301807fd8f
xingliu <xingliu@utexas.edu>
2017-09-19 13:06:40 -0700

    feat: add zGrid to construct z grid

--- geomHelper.py
+++ geomHelper.py
@@ -60,3 +60,98 @@ def init_global_geometry(suffix, pars):
 
 def q_Cy(geom_coeff):
     return geom_coeff['q'], geom_coeff['C_y']
+
+def zGrid(geom_coeff, \
+          pars, \
+          center_only = False, \
+          plot = True, \
+          edge_opt = -1):
+
+    nx = pars['nx0']
+    nz = pars['nz0']
+    if 'gBfield' in geom_coeff:
+        gBfield = geom_coeff['gBfield']
+    else:
+        gBfield = geom_coeff['Bfield']
+    if 'gjacobian' in geom_coeff:
+        gjacobian = geom_coeff['gjacobian']
+    else:
+        gjacobian = geom_coeff['jacobian']
+
+    if center_only:
+        ikx_grid = [0]
+    else:
+        ikx_grid = np.arange(- nx / 2 + 1, nx / 2 + 1)
+
+    zgrid_even_center = np.linspace(-1., 1., nz, \
+                        endpoint = False)
+    if not center_only:
+        if nx % 2 == 1:
+            zgrid_even = np.linspace(- nx, nx, nx * nz, \
+                         endpoint = False)
+        else :
+            zgrid_even = np.linspace(- (nx - 1), (nx + 1), \
+                         nx * nz, endpoint = False)
+
+    if 'edge_opt' in pars:
+      if edge_opt == -1:
+        edge_opt = pars['edge_opt']
+      else:
+        edge_opt = float(edge_opt)
+    else:
+      edge_opt = 0.
+
+    if edge_opt != 0:
+        zgrid_edge = np.zeros(nx * nz, dtype = 'float128')
+        
+        N = np.arcsinh(edge_opt * zgrid_even_center[0] * \
+            np.pi) / zgrid_even_center[0] / np.pi
+
+        zgrid_edge_center = 1. / edge_opt * \
+                            np.sinh(N * zgrid_even_center * \
+                            np.pi) / np.pi
+
+        dz = np.zeros(nz, dtype = 'float128')
+        for i in np.arange(nz / 2 + 1, nz):
+            dz[i] = zgrid_edge_center[i] - \
+                    zgrid_edge_center[i - 1]
+        for i in np.arange(nz / 2 - 1, - 1, - 1):
+            dz[i] = zgrid_edge_center[i + 1] - \
+                    zgrid_edge_center[i]
+
+        for i in ikx_grid:
+            this_zgrid_edge = i * 2. + zgrid_edge_center
+            zgrid_edge[(i - ikx_grid[0]) * \
+            nz : (i - ikx_grid[0] + 1) * nz] \
+            = this_zgrid_edge
+        if center_only:
+            zgrid = zgrid_edge_center
+        else:
+            zgrid = zgrid_edge
+    else:
+        if center_only:
+            zgrid = zgrid_even_center
+        else:
+            zgrid = zgrid_even
+
+        dz = np.ones(nz, dtype = 'float128') * 2. / nz
+
+    jacobian_center = 1. / np.pi / gjacobian / gBfield
+    
+    if not center_only:
+        jacobian = np.zeros(nx * nz, dtype = 'float128')
+        for i in ikx_grid:
+	    jacobian[(i-ikx_grid[0])*nz:(i-ikx_grid[0]+1)*nz]=\
+                jacobian_center
+    if center_only:
+        jacobian = jacobian_center
+
+    if plot:
+        plt.plot(zgrid, label = 'zgrid')
+        plt.legend(loc=2)
+        plt.show()
+        plt.plot(jacobian, label = 'jacobian')
+        plt.legend(loc=2)
+        plt.show()
+
+    return zgrid, jacobian
